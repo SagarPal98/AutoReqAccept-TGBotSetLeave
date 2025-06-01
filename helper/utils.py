bea46dc5ac18d25004e7bfd33399d1e6e817602a
arnobpaul <117028354+arnobpaul@users.noreply.github.com>
2025-06-01 18:49:37 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -1,43 +1,28 @@
 from datetime import datetime
 from pytz import timezone
 from config import Config
-from pyrogram.errors import UserNotParticipant
-from pyrogram import enums
-from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-async def send_log(b, u):
-    if Config.LOG_CHANNEL is not None:
+async def send_log(bot, user):
+    if Config.LOG_CHANNEL:
         curr = datetime.now(timezone("Asia/Kolkata"))
         date = curr.strftime('%d %B, %Y')
         time = curr.strftime('%I:%M:%S %p')
-        await b.send_message(
-            Config.LOG_CHANNEL,
-            f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: {b.mention}"
-        )
 
-
-async def is_subscribed(bot, query):
-    try:
-        user = await bot.get_chat_member(Config.AUTH_CHANNEL, query.from_user.id)
-    except UserNotParticipant:
-        pass
-    except Exception as e:
-        logger.exception(e)
-    else:
-        if user.status != enums.ChatMemberStatus.BANNED:
-            return True
-
-    return False
-
-
-async def force_sub(bot, cmd):
-    invite_link = await bot.create_chat_invite_link(int(Config.AUTH_CHANNEL))
-    buttons = [[InlineKeyboardButton(
-        text="📢 Cont. Owner to add you in Channel 📢", url="https://t.me/V_Ditu")]]
-    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
-
-    return await cmd.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+        try:
+            await bot.send_message(
+                chat_id=Config.LOG_CHANNEL,
+                text=(
+                    f"**-- New User Started the Bot --**\n\n"
+                    f"👤 **User**: {user.mention}\n"
+                    f"🆔 **ID**: `{user.id}`\n"
+                    f"🔗 **Username**: @{user.username if user.username else 'N/A'}\n"
+                    f"🗓 **Date**: `{date}`\n"
+                    f"⏰ **Time**: `{time}`"
+                )
+            )
+        except Exception as e:
+            logger.error(f"Error sending log message for user {user.id}: {e}")
